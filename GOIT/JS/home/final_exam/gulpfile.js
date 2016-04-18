4b45edc79970e8756122917f2871fd324cea050f
VictoriaDerkach <vikaderkach91@gmail.com>
2016-04-18 11:48:09 +0300

    exam

---
+++ GOIT/JS/home/final_exam/gulpfile.js
@@ -0,0 +1,67 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var cssmin = require('gulp-cssmin');
+var minifyHtml = require('gulp-minify-html');
+var image = require('gulp-image');
+var sass = require('gulp-sass');
+var del = require('del');
+
+gulp.task('clean', function() {
+  del(['build']);
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./src/css/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./build/css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/css/**/*.scss', ['sass']);
+});
+
+gulp.task('js', function() {  
+    return gulp.src('src/js/*.js')
+        .pipe(concat('script.main.js'))
+        //.pipe(uglify())
+        .pipe(gulp.dest('./build/js/'))
+        //.pipe(rename('script.main.min.js'))
+        //.pipe(gulp.dest('./js/'))
+});
+
+gulp.task('css', function() {  
+    return gulp.src(['./css/reset.css', './css/style.css'])
+        .pipe(concat('style.main.min.css'))
+        .pipe(cssmin())
+        .pipe(gulp.dest('./build/css/'))
+        
+        //.pipe(rename('style.main.min.css'))
+        //.pipe(gulp.dest('./css/'))
+});
+
+gulp.task('html', function() {  
+    return gulp.src(['src/html/header.html', 'src/html/content.html', 'src/html/footer.html'])
+        .pipe(concat('index.html'))
+        .pipe(minifyHtml()) 
+        .pipe(gulp.dest('./'))
+});
+
+gulp.task('img', function () {
+  gulp.src('./src/img/*')
+    .pipe(image({
+      pngquant: true,
+      optipng: false,
+      zopflipng: true,
+      advpng: true,
+      jpegRecompress: false,
+      jpegoptim: true,
+      mozjpeg: true,
+      gifsicle: true,
+      svgo: true
+    }))
+    .pipe(gulp.dest('./build/img'));
+});
+
+gulp.task('default', ['sass', 'js', 'css', 'html', 'img'], function() {
+});
---
+++ GOIT/JS/home/final_exam/src/js/script.js
@@ -0,0 +1,55 @@
+$(function () {
+    $('.jcarousel')
+    .jcarousel({
+        wrap: 'circular'
+    })
+    .jcarouselAutoscroll({
+    	autostart: true,
+    	target: '+=1',
+        interval: 2000
+    });
+
+
+    getImages('', 7);
+
+    $('#search').click(function (e) {
+        e.preventDefault();
+        getImages($('#input_search').val(), 7);
+        return false;
+    });
+});
+
+function getImages(query, amount) {
+    query = encodeURIComponent(query);
+    $.ajax({
+    	cache: false,
+        type: "GET",
+        url: 'http://api.pixplorer.co.uk/image?&amount=' + amount + '&size=10&word=' + query,
+        dataType: "json",
+        success: function (data) {
+        	if (data.status !== 'success') {
+        		alert('Get images - failed!');
+        		return;
+        	}
+        	//console.log(data);
+        	var words = [], images = [];
+            for (i = 0; i <= ((data.images.length - 1)); i++) {
+                images[i] = data.images[i].imageurl;
+                words[i] = data.images[i].word;
+            }
+        	setImages(images, words);
+        }
+    })
+}
+
+function setImages(images, words) {
+    var imagesList = tmpl($('#img-list').html(), {images: images, words: words});
+    $('.ideas_image_wrapper')
+    .html('')
+    .append(imagesList)
+	.masonry({
+		percentPosition: true,
+		columnWidth: '.grid-item-x1',
+  		itemSelector: '.ideas_image',
+	});
+}
